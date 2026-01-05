c442d06b44a6b9e01849bd71374cb013fbe41bee
shimjh222-creator <shimjh222@gmail.com>
2026-01-05 08:02:48 +0700

    Update login.py

--- login.py
+++ login.py
@@ -38,7 +38,7 @@ def open_login_screen(root):
             result = cur.fetchone()
 
         if result:
-            messagebox.showinfo("Success", "Login Successful! ✅\nWelcome to Coffee Heaven! ☕")
+            messagebox.showinfo("Success", "Login Successful! ✅\nWelcome to Coffee Brew! ☕")
             open_menu_main(root)
         else:
             messagebox.showerror("Error", "Invalid login")
